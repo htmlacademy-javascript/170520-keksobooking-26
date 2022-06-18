66574bd5c30d9750902ade5880c58c08f3ccbf42
Anton Shchukin <Tony@Laptop.local>
2022-06-18 16:14:49 +0700

    черновик random float in range

--- js/main.js
+++ js/main.js
@@ -7,3 +7,29 @@ function randomIntInRange(min, max) {
   }
   return Math.round(Math.random() * (max - min)) + min;
 }
+
+
+function randomFloatInRange(min, max, precision) {
+
+  if ( !min && min !== 0 || !max && max !==0 || typeof min !== 'number' || typeof max !== 'number' || min < 0 || max < 0 || precisionWithRounding(min, precision) >= precisionWithRounding(max,precision)) {
+    return null;
+  }
+
+  return precisionWithRounding( (Math.random() * (max - min) + min), precision);
+}
+
+
+
+/*
+* toPrecision() не подходит потому что даёт 1.009
+*/
+function precisionWithRounding(number, precision) {
+  const factor = Math.pow(10, precision);
+  return Math.round(number * factor) / factor;
+}
+
+
+
+for ( let i=0; i<30; i++ ) {
+  console.log(randomFloatInRange(1.46,1.47, 2));
+}
